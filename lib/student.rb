94aa4dfa9c57bc36cec8c0b76b98e82f1443d864
Luis Zabala <luis.zabala076@gmail.com>
2019-02-18 17:57:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -15,10 +15,13 @@ end
 def self.create_table
   DB[:conn].execute(
     "CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)")
+    #DB[:conn] 'connect db' .execute > "CREATE TABLE [table name]"
 end
   
 def self.drop_table
   DB[:conn].execute("DROP TABLE students")
+  
+    #DB[:conn] '.execute' > 'DROP TABLE [table name]'
 end
 
 def save
@@ -28,12 +31,16 @@ def save
   
   DB[:conn].execute(sql, self.name, self.grade)
   @id = DB[:conn].execute("SELECT id FROM students WHERE id = (SELECT (id) FROM students);")[0][0]
+    #Insert into SQL with here doc
+    #DB[:conn] '.execute' > SELECT id FROM students WHERE id = [select id from db]
 end
 
 def self.create(student)
   student = self.new(student[:name], student[:grade])
   student.save
   student
+  
+    #create db from sql utilizing db [:value, :value]
 end
 end
 
